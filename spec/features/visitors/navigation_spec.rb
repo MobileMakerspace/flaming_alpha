4cba59e76d13d68aea4782f5029b6adf79132668
Dana Spisak <dana_spisak@mentor.com>
2016-11-23 14:00:53 -0600

    update spec for removing registerable

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -7,12 +7,12 @@ feature 'Navigation links', :devise do
   # Scenario: View navigation links
   #   Given I am a visitor
   #   When I visit the home page
-  #   Then I see "home," "sign in," and "sign up"
+  #   Then I see "home," "sign in," and "about"
   scenario 'view navigation links' do
     visit root_path
     expect(page).to have_content 'Home'
     expect(page).to have_content 'Sign in'
-    expect(page).to have_content 'Sign up'
+    expect(page).to have_content 'About'
   end
 
 end
