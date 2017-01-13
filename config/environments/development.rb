ffe347556886b4cabdc2a5b3c14349d7e08c8bdf
Dana Spisak <dana_spisak@mentor.com>
2017-01-13 12:44:16 -0600

    Setup for using mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  '''
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -51,6 +52,9 @@ Rails.application.configure do
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
+  '''
+  # Use mailcatcher locally in development
+  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
