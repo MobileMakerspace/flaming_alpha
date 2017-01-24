a515bb851d3f2719750bd5fbd9f21e1ccd5c78a3
Dana Spisak <dana_spisak@mentor.com>
2017-01-24 15:15:23 -0600

    Disable registration in production, hacky

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def new
-    puts "Override!"
-    super
+    if Rails.env.production?
+      flash[:info] = 'Registrations are not open yet, but please check back soon'
+      redirect_to root_path
+    else
+      super
+    end
+  end
+  def create
+    if Rails.env.production?
+      flash[:info] = 'Registrations are not open yet, but please check back soon'
+      redirect_to root_path
+    else
+      super
+    end
   end
 end
