9f92a947a43e75f17917ed3dd6a50ddf542a904e
Dana Spisak <dana_spisak@mentor.com>
2017-01-30 15:25:24 -0600

    Disable mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
