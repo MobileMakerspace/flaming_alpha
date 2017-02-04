2f5d7c7decb87091ad24047334ee544f85a676cc
Dana Spisak <dana_spisak@mentor.com>
2017-02-03 22:39:02 -0600

    Added balance to subscribers list

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable, :validatable
+
+  def balance
+    DoubleEntry.account(:accounts_receivable, :scope => self).balance
+  end
 end
