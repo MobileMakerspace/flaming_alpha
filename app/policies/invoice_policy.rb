0f439befd56fab8bd4da94438ae8ffbc741ef658
Dana Spisak <dana_spisak@mentor.com>
2017-02-13 10:46:48 -0600

    Cut down on duplicate policy models

--- app/policies/invoice_policy.rb
+++ app/policies/invoice_policy.rb
@@ -1,18 +1,2 @@
-class InvoicePolicy
-  attr_reader :current_user, :model
-
-  def initialize(current_user, model)
-    @current_user = current_user
-    @scope = model
-  end
-
-  def update?
-    @current_user.admin?
-  end
-
-  def show?
-    @current_user.admin?
-  end
-
-
+class InvoicePolicy <FinancialPolicy
 end
--- app/policies/payment_policy.rb
+++ app/policies/payment_policy.rb
@@ -1,18 +1,2 @@
-class PaymentPolicy
-  attr_reader :current_user, :model
-
-  def initialize(current_user, model)
-    @current_user = current_user
-    @scope = model
-  end
-
-  def update?
-    @current_user.admin?
-  end
-
-  def show?
-    @current_user.admin?
-  end
-
-
+class PaymentPolicy< FinancialPolicy
 end
